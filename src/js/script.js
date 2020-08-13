1ce06b1d1581d8d4878a7375a6bf67404904f98d
Artem <artem.xolod.90@gmail.com>
2020-08-13 19:54:17 +0300

    Адаптация 4 блока

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
   $(".carusel__inner").slick({
     speed: 1200,
-    adaptiveHeight: true,
+    adaptiveHeight: false,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="./icons/arrow_left.png" ></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="./icons/arrow_right.png" ></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
+      }
+    ]
   });
 });
