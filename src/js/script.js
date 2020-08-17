a0c55c135cc7f262f58653b25e6a2cc138558d91
Artem <artem.xolod.90@gmail.com>
2020-08-17 11:45:25 +0300

    Маска для ввода номера телефона в формах

--- src/js/script.js
+++ src/js/script.js
@@ -91,4 +91,6 @@ $(document).ready(function () {
   validation("#consultation form");
   validation("#order form");
   validation("#consultation-form");
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
