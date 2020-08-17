2821aa7d740a7f396b38c6e676983d61e3ff4350
Artem <artem.xolod.90@gmail.com>
2020-08-17 20:17:22 +0300

    Анимации

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Pulse-Project
 Домашний проект для практики.
 
-Используемые технологии/библиотеки: SCSS, CSS3, HTML5, БЭМ, JavaScript, slick.js, Git, Gulp, JQuery.
+Используемые технологии/библиотеки: SCSS, CSS3, HTML5, БЭМ, JavaScript, slick.js, Git, Gulp, JQuery, mailer.php, wow.js, animate.css.
--- src/js/script.js
+++ src/js/script.js
@@ -122,9 +122,20 @@ $(document).ready(function () {
     }
   });
 
-  $("a[href^='#']").click(function () {
-    const _href = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
-    return false;
-  });
+  // SlowScrolling
+
+  function slowScrolling(item) {
+    $(`a[href=${item}]`).click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+    });
+  }
+
+  slowScrolling("#up");
+  slowScrolling("#down");
+
+  // wow.js
+
+  new WOW().init();
 });
