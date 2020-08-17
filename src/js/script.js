3fa768375a5db05719c49d426ca35afc24605fe4
Artem <artem.xolod.90@gmail.com>
2020-08-17 13:38:26 +0300

    Возможность отправлять данные с форм на почту

--- src/js/script.js
+++ src/js/script.js
@@ -92,5 +92,23 @@ $(document).ready(function () {
   validation("#order form");
   validation("#consultation-form");
 
-  $('input[name=phone]').mask("+7 (999) 999-99-99");
+  $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+  // sending letters to mail
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "./mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn();
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
 });
